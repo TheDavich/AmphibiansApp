3036a4600ebe11030d8bc7c79090dcf5629acaa6
TheDavich <k.davud@yandex.ru>
2023-05-25 14:23:02 +0200

    UI update

--- app/src/main/java/com/example/amphibiansapp/ui/theme/AmphibiansApp.kt
+++ app/src/main/java/com/example/amphibiansapp/ui/theme/AmphibiansApp.kt
@@ -1,21 +1,25 @@
 package com.example.amphibiansapp.ui.theme
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.amphibiansapp.R
 import com.example.amphibiansapp.model.Amphibians
 import com.example.amphibiansapp.ui.theme.screens.AmphibiansViewModel
 import com.example.amphibiansapp.ui.theme.screens.HomeScreen
+import androidx.compose.material.Scaffold
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -23,15 +27,19 @@ fun AmphibiansApp(
     modifier: Modifier = Modifier
 ) {
     Scaffold(
-        modifier = modifier
-            .fillMaxSize(),
-        topBar = { TopAppBar(title = { Text(stringResource(R.string.app_name)) }) }
+        modifier = modifier.fillMaxSize(),
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(stringResource(R.string.app_name))
+                }
+            )
+        },
     ) {
         Surface(
             modifier = modifier
                 .fillMaxSize()
                 .padding(it),
-            color = MaterialTheme.colorScheme.background
         ) {
             val amphibiansViewModel: AmphibiansViewModel = viewModel(
                 factory = AmphibiansViewModel.Factory
--- app/src/main/java/com/example/amphibiansapp/ui/theme/screens/HomeScreen.kt
+++ app/src/main/java/com/example/amphibiansapp/ui/theme/screens/HomeScreen.kt
@@ -1,18 +1,31 @@
 package com.example.amphibiansapp.ui.theme.screens
 
+import android.widget.Space
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.SnackbarDefaults.backgroundColor
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
@@ -78,45 +91,80 @@ fun ErrorScreen(
 fun AmphibianCard(
     amphibians: Amphibians,
     modifier: Modifier = Modifier
+
 ) {
-    Column(
+
+    val surfaceColor = androidx.compose.material.MaterialTheme.colors.surface
+    val backgroundColor = androidx.compose.material.MaterialTheme.colors.background
+    val textColor = androidx.compose.material.MaterialTheme.colors.onSurface
+
+    Card(
         modifier = modifier
-            .fillMaxSize()
-            .padding(4.dp),
-        verticalArrangement = Arrangement.SpaceBetween
+            .padding(16.dp)
+            .background(backgroundColor)
     ) {
-        Row(
+        Column(
             modifier = modifier
-                .padding(4.dp)
+                .fillMaxSize()
+                .clip(MaterialTheme.shapes.medium)
+                .background(surfaceColor)
+                .padding(4.dp),
+            verticalArrangement = Arrangement.SpaceBetween,
         ) {
-            Text(
-                text = amphibians.name
-            )
-            Text(
-                text = "(${amphibians.type})"
-            )
-            Text(text = amphibians.description)
+            Row(
+                modifier = modifier
+                    .padding(4.dp)
+            ) {
+                Text(
+                    text = amphibians.name + " (" + amphibians.type + ")",
+                    style = androidx.compose.material.MaterialTheme.typography.h2,
+                    color = textColor,
+                )
+            }
+            Spacer(modifier = modifier.height(8.dp))
+            Column(
+                modifier = modifier
+                    .fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                AsyncImage(
+                    modifier = modifier
+                        .fillMaxWidth()
+                        .clip(MaterialTheme.shapes.medium)
+                        .shadow(4.dp),
+
+                    model = ImageRequest.Builder(context = LocalContext.current)
+                        .data(amphibians.imgSrc)
+                        .crossfade(true)
+                        .build(),
+                    contentDescription = amphibians.type,
+                    contentScale = ContentScale.Crop,
+                    error = painterResource(id = R.drawable.ic_broken_image),
+                    placeholder = painterResource(id = R.drawable.loading_img),
+                )
+                Spacer(modifier = modifier.height(8.dp))
+                Text(
+                    text = amphibians.description,
+                    modifier = modifier
+                        .padding(4.dp),
+                    color = textColor,
+                    style = androidx.compose.material.MaterialTheme.typography.body1
+                )
+            }
+
         }
-
-        AsyncImage(
-            model = ImageRequest.Builder(context = LocalContext.current)
-                .data(amphibians.imgSrc)
-                .crossfade(true)
-                .build(),
-            contentDescription = amphibians.type,
-            contentScale = ContentScale.FillBounds,
-            error = painterResource(id = R.drawable.ic_broken_image),
-            placeholder = painterResource(id = R.drawable.loading_img)
-        )
     }
+    Spacer(modifier = modifier.height(16.dp))
 }
-
 @Composable
 fun ListScreen(
     amphibians: List<Amphibians>,
     modifier: Modifier = Modifier
 ) {
-    LazyColumn {
+    LazyColumn(
+        modifier = modifier
+            .background(androidx.compose.material.MaterialTheme.colors.background)
+    ) {
         items(amphibians.size) { index ->
             AmphibianCard(amphibians = amphibians[index])
         }
